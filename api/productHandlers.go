4204ffbb5d49494c170476be133d966d9eade909
ilyakharenkov <ilyakharenkov@gmail.com>
2026-06-29 21:42:30 +0300

    Refactoring

--- api/productHandlers.go
+++ api/productHandlers.go
@@ -1,11 +1,14 @@
 package api
 
 import (
+	"encoding/json"
 	"inventoiry-service/internal/service"
 	"inventoiry-service/internal/service/dto"
+	"net/http"
 )
 
 type ProductHandler interface {
+	RegisterRoutes(mux *http.ServeMux)
 	CreateProduct(request *dto.Product) *dto.Product
 	FindProductBySku(sku string) *dto.Product
 	AdjustStock(sku string, request *dto.Stock) *dto.Product
@@ -17,6 +20,60 @@ func NewProductHttpHandler(service service.ProductService) ProductHandler {
 
 type productHttpHandler struct {
 	service service.ProductService
+}
+
+func (handler *productHttpHandler) RegisterRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			var requestBody dto.Product
+			if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+
+			response := handler.CreateProduct(&requestBody)
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
+			if err := json.NewEncoder(w).Encode(response); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		case http.MethodGet:
+			sku := r.URL.Query().Get("sku")
+			response := handler.FindProductBySku(sku)
+
+			if response == nil {
+				http.Error(w, "product by sku not found", http.StatusNotFound)
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
+			if err := json.NewEncoder(w).Encode(response); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	})
+
+	mux.HandleFunc("/products/stock", func(w http.ResponseWriter, r *http.Request) {
+		var requestBody dto.Stock
+		if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+		sku := r.URL.Query().Get("sku")
+		response := handler.AdjustStock(sku, &requestBody)
+
+		if response == nil {
+			http.Error(w, "product by sku not found", http.StatusNotFound)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
 }
 
 func (handler *productHttpHandler) CreateProduct(request *dto.Product) *dto.Product {
--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"inventoiry-service/api"
 	"inventoiry-service/internal/repository"
 	"inventoiry-service/internal/service"
-	"inventoiry-service/internal/service/dto"
 	"net/http"
 )
 
@@ -15,59 +13,10 @@ func main() {
 	productService := service.NewProductService(productRepository)
 	productHandler := api.NewProductHttpHandler(productService)
 
-	http.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			var requestBody dto.Product
-			if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
-				http.Error(w, err.Error(), http.StatusBadRequest)
-			}
+	mux := http.NewServeMux()
+	productHandler.RegisterRoutes(mux)
 
-			response := productHandler.CreateProduct(&requestBody)
-
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusCreated)
-			if err := json.NewEncoder(w).Encode(response); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
-		case http.MethodGet:
-			sku := r.URL.Query().Get("sku")
-			response := productHandler.FindProductBySku(sku)
-
-			if response == nil {
-				http.Error(w, "product by sku not found", http.StatusNotFound)
-			}
-
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusCreated)
-			if err := json.NewEncoder(w).Encode(response); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
-		default:
-			w.WriteHeader(http.StatusMethodNotAllowed)
-		}
-	})
-
-	http.HandleFunc("/products/stock", func(w http.ResponseWriter, r *http.Request) {
-		var requestBody dto.Stock
-		if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-		sku := r.URL.Query().Get("sku")
-		response := productHandler.AdjustStock(sku, &requestBody)
-
-		if response == nil {
-			http.Error(w, "product by sku not found", http.StatusNotFound)
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		if err := json.NewEncoder(w).Encode(response); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
-
-	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
+	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
 		fmt.Println(err)
 	}
 }
